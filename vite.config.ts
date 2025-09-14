a34dd5989203f9de924de9c913000ae20c6318ec
shrinivasshah <shrinivasshah05@gmail.com>
2025-09-14 12:35:02 +0530

    added netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import netlify from '@netlify/vite-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), netlify()],
 });
